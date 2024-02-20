708999501ecfdf05c3445b7a582d53eb15363aba
Ramzi Osta <ramziosta@gmail.com>
2024-02-19 21:57:12 -0500

    hardhat project

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+};
+
+
+// this is added to be able to use yarn hardhat accounts command to get a list of fake accounts
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
